b7a0d67bb95a590791317c77b790735f3ace0964
MongusChoi <Mongus.C@gmail.com>
2023-02-24 18:34:57 +0900

    [최명수] 02-24 infowindow 관련 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from "@react-google-maps/api";
 import React, { useEffect } from "react";
 const containerStyle = {
   width: "400px",
@@ -18,6 +18,7 @@ const MyComponent = () => {
   });
 
   const [map, setMap] = React.useState(null);
+  const [isOpenWindow, setIsOpenWidow] = React.useState(false)
 
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
@@ -31,16 +32,35 @@ const MyComponent = () => {
     setMap(null);
   }, []);
 
+  /**
+   * 
+   * @param {google.maps.MapMouseEvent} e 
+   */
+  const onClickMarker = e => setIsOpenWidow(true)
+
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={10}
+      zoom={15}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
-      <Marker position={{ lat: -3.745, lng: -38.523 }}></Marker>
+      <Marker onClick={onClickMarker} position={{ lat: -3.745, lng: -38.523 }}>
+        {isOpenWindow && 
+        <InfoWindow onCloseClick={() => setIsOpenWidow(false)}>
+          <div style={{ display: 'flex' ,width: 150, height: 70 }}>
+            <div style={{ marginRight: 10 }}>
+              <img src='/response.jpg' height={65} alt='place img'/>
+            </div>
+            <div>
+              <div style={{ fontSize: 20, fontWeight: "bold" }}>test</div>
+              <div>infowindow test</div>
+            </div>
+          </div>
+        </InfoWindow>}
+      </Marker>
     </GoogleMap>
   ) : (
     <></>
